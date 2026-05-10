d27a4545e0371299a582febb701b016447965d67
muhamedrar <mohamedahmed0218@gmail.com>
2026-05-10 07:41:25 +0300

    feat(db): add database configuration and connection setup

--- src/helpers/settings.py
+++ src/helpers/settings.py
@@ -1,16 +1,32 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
+from pathlib import Path
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env',env_file_encoding='utf-8')
+    model_config = SettingsConfigDict('.env',env_file_encoding='utf-8',extra='ignore')
 
 
     APP_NAME:str
     APP_VERSION:str
     
-    POSTGERS_USER=str
-    POSTGERS_PASSWORD=str
-    POSTGERS_URL=str
+    POSTGRES_USER:str
+    POSTGRES_PASSWORD:str
+    POSTGRES_URL:str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str 
+
+    DRIVER: str = "psycopg2"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        driver_part = f"+{self.DRIVER}" if self.DRIVER else ""
+        
+        return (
+            f"postgresql{driver_part}://"
+            f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_URL}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
 
     
 @lru_cache()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routes.info import router as info_router
+from helpers.settings import get_settings
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings = get_settings()
+    
+    app.state.settings = settings
+    print(f"Starting {settings.APP_NAME} version {settings.APP_VERSION}")
+
+    yield
+
+    print(f"Shutting down {settings.APP_NAME}")
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(info_router)
