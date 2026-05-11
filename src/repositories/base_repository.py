95273e4351121d65e16f7cfe507b344174c92f52
muhamedrar <mohamedahmed0218@gmail.com>
2026-05-11 04:46:22 +0300

    remove base

--- src/repositories/base_repository.py
+++
@@ -1,9 +0,0 @@
-
-from helpers.settings import get_settings
-
-
-class BaseRepository:
-    def __init__(self, db_client:object):
-        self.db_client = db_client
-        self.settings = get_settings()
-        
