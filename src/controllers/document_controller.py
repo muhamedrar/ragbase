88e2be5b2e266d9194fd60bdcc1229a53cddd876
muhamedrar <mohamedahmed0218@gmail.com>
2026-05-12 09:08:48 +0300

    feat(api): implement document controller and routes for document upload

---
+++ src/controllers/document_controller.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+
+class DocumentController:
+    # extract meta data 
+    # hash function and validation
+    # document size validation in settings
+    pass
--- src/routes/documentsRoute.py
+++ src/routes/documentsRoute.py
@@ -5,6 +5,12 @@ from fastapi.responses import JSONResponse
 from models.enums.ResponseEnums import ResponseEnums
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.dependencies import get_db_session
+from controllers.department_controller import DepartmentController
+from repositories.document_repository import  DocumentRepository
+from repositories.department_repository import  DepartmentRepository
+
+
+
 
 router = APIRouter(
     prefix="/documents",
@@ -19,4 +25,21 @@ async def upload_document(
   session: AsyncSession = Depends(get_db_session),
 ):
 
- pass
+  document_repo = DocumentRepository(session=session)
+  department_repo = DepartmentRepository(session=session)
+  department_controller = DepartmentController()
+  
+  if not await department_repo.is_department_exists(id=department_id):
+    return JSONResponse(
+      status_code=status.HTTP_404_NOT_FOUND,
+      content={
+        'message': ResponseEnums.DEPARTMENT_NOT_FOUND.value
+      }
+    )
+  
+
+  department = await department_repo.get_department_by_id(department_id=department_id)
+  department_path = await department_controller.get_or_create_department_path(department_name=department.name)
+
+
+  pass
